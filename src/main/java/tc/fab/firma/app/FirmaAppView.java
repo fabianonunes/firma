cb48a9d362505dcf8adacf6699c1b49f23125dfc
Fabiano Nunes <fabiano@nunes.me>
2013-02-22 12:29:57 -0300

    rename view variables

--- src/main/java/tc/fab/firma/app/FirmaAppView.java
+++ src/main/java/tc/fab/firma/app/FirmaAppView.java
@@ -8,7 +8,12 @@ import java.awt.Dimension;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.swing.ActionMap;
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle;
 
 import org.jdesktop.application.FrameView;
 import org.jdesktop.application.ResourceMap;
@@ -42,133 +47,105 @@ public class FirmaAppView extends FrameView implements AppView {
 
 	private void initComponents() {
 
-		mainPanel = new javax.swing.JPanel();
-		jButton2 = new javax.swing.JButton();
-		jButton3 = new javax.swing.JButton();
-		jButton1 = new javax.swing.JButton();
-		addFiles = new javax.swing.JButton();
-		settings = new javax.swing.JButton();
-		jScrollPane2 = new javax.swing.JScrollPane();
-		jTable1 = new JFileTable();
-		jButton6 = new javax.swing.JButton();
-		jButton7 = new javax.swing.JButton();
+		mainPanel = new JPanel();
+		signFiles = new JButton();
+		removeFile = new JButton();
+		addFolder = new JButton();
+		addFiles = new JButton();
+		settings = new JButton();
+		scrollPane = new JScrollPane();
+		fileTable = new JFileTable();
+		showDropArea = new JButton();
+		jButton7 = new JButton();
 
-		mainPanel.setName("mainPanel"); // NOI18N
+		mainPanel.setName("mainPanel");
 
 		ActionMap actionMap = controller.getActionMap();
 		ResourceMap resourceMap = context.getAppContext().getResourceMap();
 
-		jButton2.setAction(actionMap.get("signSelection")); // NOI18N
-		jButton2.setIcon(resourceMap.getIcon("jButton2.icon")); // NOI18N
-		jButton2.setName("jButton2"); // NOI18N
+		signFiles.setAction(actionMap.get("signSelection"));
+		removeFile.setAction(actionMap.get("removeSelecteds"));
+		addFolder.setAction(actionMap.get("getSelection"));
+		addFiles.setAction(actionMap.get(AppController.ACTION_FILES_ADD));
+		settings.setAction(actionMap.get(AppController.ACTION_SETTINGS));
+		showDropArea.setAction(actionMap.get("showDropBox"));
 
-		jButton3.setAction(actionMap.get("removeSelecteds")); // NOI18N
-		jButton3.setIcon(resourceMap.getIcon("jButton3.icon")); // NOI18N
-		jButton3.setText(resourceMap.getString("jButton3.text")); // NOI18N
-		jButton3.setName("jButton3"); // NOI18N
+		scrollPane.setViewportView(fileTable);
 
-		jButton1.setAction(actionMap.get("getSelection")); // NOI18N
-		jButton1.setIcon(resourceMap.getIcon("jButton1.icon")); // NOI18N
-		jButton1.setText(resourceMap.getString("jButton1.text")); // NOI18N
-		jButton1.setName("jButton1"); // NOI18N
+		jButton7.setAction(actionMap.get("previewFile"));
+		jButton7.setName("jButton7");
 
-		addFiles.setAction(actionMap.get(AppController.ACTION_FILES_ADD)); // NOI18N
-		settings.setAction(actionMap.get(AppController.ACTION_SETTINGS)); // NOI18N
-
-		jScrollPane2.setName("jScrollPane2"); // NOI18N
-
-		jTable1.setName("jTable1"); // NOI18N
-		jScrollPane2.setViewportView(jTable1);
-
-		jButton6.setAction(actionMap.get("showDropBox")); // NOI18N
-		jButton6.setIcon(resourceMap.getIcon("jButton6.icon")); // NOI18N
-		jButton6.setText(resourceMap.getString("jButton6.text")); // NOI18N
-		jButton6.setName("jButton6"); // NOI18N
-
-		jButton7.setAction(actionMap.get("previewFile")); // NOI18N
-		jButton7.setName("jButton7"); // NOI18N
-
-		javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
+		GroupLayout mainPanelLayout = new GroupLayout(mainPanel);
 		mainPanel.setLayout(mainPanelLayout);
-		mainPanelLayout
-			.setHorizontalGroup(mainPanelLayout.createParallelGroup(
-				javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(
-					mainPanelLayout
-						.createSequentialGroup()
-						.addContainerGap()
-						.addGroup(
-							mainPanelLayout
-								.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-								.addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE,
-									623, Short.MAX_VALUE)
-								.addGroup(
-									mainPanelLayout
-										.createSequentialGroup()
-										.addComponent(jButton1)
-										.addPreferredGap(
-											javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-										.addComponent(addFiles)
-										.addPreferredGap(
-											javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-										.addComponent(settings)
-										.addPreferredGap(
-											javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-											203, Short.MAX_VALUE).addComponent(jButton6))
-								.addGroup(
-									mainPanelLayout
-										.createSequentialGroup()
-										.addComponent(jButton2)
-										.addPreferredGap(
-											javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-										.addComponent(jButton7)
-										.addPreferredGap(
-											javax.swing.LayoutStyle.ComponentPlacement.RELATED,
-											292, Short.MAX_VALUE).addComponent(jButton3)))
-						.addContainerGap()));
-		mainPanelLayout.setVerticalGroup(mainPanelLayout.createParallelGroup(
-			javax.swing.GroupLayout.Alignment.LEADING).addGroup(
+		mainPanelLayout.setHorizontalGroup(mainPanelLayout.createParallelGroup(
+			GroupLayout.Alignment.LEADING).addGroup(
 			mainPanelLayout
 				.createSequentialGroup()
 				.addContainerGap()
 				.addGroup(
 					mainPanelLayout
-						.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(addFiles, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(settings, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE))
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-				.addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 421,
-					Short.MAX_VALUE)
-				.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+						.createParallelGroup(GroupLayout.Alignment.LEADING)
+						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 623, Short.MAX_VALUE)
+						.addGroup(
+							mainPanelLayout
+								.createSequentialGroup()
+								.addComponent(addFolder)
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(addFiles)
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(settings)
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 203,
+									Short.MAX_VALUE).addComponent(showDropArea))
+						.addGroup(
+							mainPanelLayout
+								.createSequentialGroup()
+								.addComponent(signFiles)
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+								.addComponent(jButton7)
+								.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 292,
+									Short.MAX_VALUE).addComponent(removeFile))).addContainerGap()));
+		mainPanelLayout.setVerticalGroup(mainPanelLayout.createParallelGroup(
+			GroupLayout.Alignment.LEADING).addGroup(
+			mainPanelLayout
+				.createSequentialGroup()
+				.addContainerGap()
 				.addGroup(
 					mainPanelLayout
-						.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-						.addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addComponent(jButton7, javax.swing.GroupLayout.PREFERRED_SIZE, 28,
-							javax.swing.GroupLayout.PREFERRED_SIZE)).addContainerGap()));
+						.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(addFolder, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE)
+						.addComponent(addFiles, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE)
+						.addComponent(settings, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE)
+						.addComponent(showDropArea, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE))
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 421, Short.MAX_VALUE)
+				.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+				.addGroup(
+					mainPanelLayout
+						.createParallelGroup(GroupLayout.Alignment.BASELINE)
+						.addComponent(signFiles, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE)
+						.addComponent(removeFile, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE)
+						.addComponent(jButton7, GroupLayout.PREFERRED_SIZE, 28,
+							GroupLayout.PREFERRED_SIZE)).addContainerGap()));
 
 		setComponent(mainPanel);
 	}
 
-	private javax.swing.JButton jButton1;
-	private javax.swing.JButton jButton2;
-	private javax.swing.JButton jButton3;
-	private javax.swing.JButton addFiles;
-	private javax.swing.JButton settings;
-	private javax.swing.JButton jButton6;
-	private javax.swing.JButton jButton7;
-	private javax.swing.JScrollPane jScrollPane2;
-	private JFileTable jTable1;
-	private javax.swing.JPanel mainPanel;
+	private JButton addFolder;
+	private JButton signFiles;
+	private JButton removeFile;
+	private JButton addFiles;
+	private JButton settings;
+	private JButton showDropArea;
+	private JButton jButton7;
+	private JScrollPane scrollPane;
+	private JFileTable fileTable;
+	private JPanel mainPanel;
 	private SignerOptionDialog optionsDialog;
 	private DialogDrop dd;
 
@@ -183,14 +160,14 @@ public class FirmaAppView extends FrameView implements AppView {
 	}
 
 	public JFileTable getFileTable() {
-		return jTable1;
+		return fileTable;
 	}
 
 	public void postInitComponents() {
 
-		jTable1.getTableHeader().setPreferredSize(
-			new Dimension(jTable1.getTableHeader().getWidth(), 21));
-		jTable1.setRowHeight(24);
+		fileTable.getTableHeader().setPreferredSize(
+			new Dimension(fileTable.getTableHeader().getWidth(), 21));
+		fileTable.setRowHeight(24);
 
 	}
 
