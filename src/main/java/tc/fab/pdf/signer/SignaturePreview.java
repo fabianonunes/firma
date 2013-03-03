9dec569e7dc0fd6fc5c25881d3d3fb0aba6ab9a2
Fabiano Nunes <fabiano@nunes.me>
2013-03-03 13:11:54 -0300

    change layer2text - wip

--- src/main/java/tc/fab/pdf/signer/SignaturePreview.java
+++ src/main/java/tc/fab/pdf/signer/SignaturePreview.java
@@ -9,11 +9,15 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.GeneralSecurityException;
 import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Map;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 
 import com.Ostermiller.util.CircularByteBuffer;
+import com.google.common.base.Joiner;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Rectangle;
@@ -22,6 +26,7 @@ import com.itextpdf.text.pdf.PdfSignatureAppearance;
 import com.itextpdf.text.pdf.PdfSignatureAppearance.RenderingMode;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.security.CertificateInfo;
 
 public class SignaturePreview {
 
@@ -44,10 +49,9 @@ public class SignaturePreview {
 
 		Signer signer = new Signer(null);
 
-		try (
-			InputStream in = createBuffer.getInputStream();
-			OutputStream out = createBuffer.getOutputStream();
-		) {
+		try (InputStream in = createBuffer.getInputStream();
+			OutputStream out = createBuffer.getOutputStream();) {
+
 			Document doc = new Document(new Rectangle(width, height));
 			PdfWriter writer = PdfWriter.getInstance(doc, out);
 			doc.open();
@@ -58,13 +62,18 @@ public class SignaturePreview {
 			PdfReader reader = new PdfReader(in);
 			createBuffer.clear();
 
+			Map<String, ArrayList<String>> values = CertificateInfo.getSubjectFields((X509Certificate) cert).getFields();
+			String layer2Text = Joiner.on(", ").withKeyValueSeparator("=").join(values);
+
 			PdfStamper stamper = PdfStamper.createSignature(reader, out, '\0');
+
 			PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
 			appearance.setVisibleSignature(new Rectangle(0, 0, width, height), 1, "Signature1");
 			appearance.setCertificate(cert);
 			appearance.setRenderingMode(RenderingMode.NAME_AND_DESCRIPTION);
+			appearance.setLayer2Text(layer2Text);
 			signer.getSignableStream(appearance, new Certificate[] { cert });
-
+			
 			PDDocument pddoc = PDDocument.load(in);
 			PDPage page = (PDPage) pddoc.getDocumentCatalog().getAllPages().get(0);
 			BufferedImage buffered = page.convertToImage();
